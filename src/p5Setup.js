bbfd2cf81fcae7c989175e3e1db5e4053511b3c3
Lars Nordhagen <Larsanordhagen@gmail.com>
2023-10-25 14:05:32 -0700

    started menu

--- src/p5Setup.js
+++ src/p5Setup.js
@@ -1,6 +1,15 @@
+// Used to load all assets such as images and fonts
+let font;
+let buttonImg;
+
+function preload() {
+    font = loadFont("./assets/fonts/Nunito-Light.ttf");
+    buttonImg = loadImage("./assets/images/green-button.png");
+}
+
 // The setup function that initializes the p5 code and canvas
 // Automatically called
 function setup() {
     createCanvas(400, 500);
-    changePage("screen2");
+    changePage("menu");
 }
---
+++ src/pages/menu.js
@@ -0,0 +1,26 @@
+{
+
+let playButton, progressButton, settingsButton;
+
+background(150);
+
+textSize(80);
+textAlign(CENTER, TOP);
+textFont(font);
+text("Strelpe", 0, 30, width);
+
+playButton = createButton("Menu");
+playButton.position(width/2, 100);
+
+progressButton = createButton("Progress");
+progressButton.position(width/2, 150);
+
+settingsButton = createButton("Settings");
+settingsButton.position(width/2, 200);
+
+function draw() {
+}
+
+
+
+}
