12d1055df899dab373e9acd4b0b208175f61f8c5
Lars Nordhagen <Larsanordhagen@gmail.com>
2023-10-24 21:15:53 -0700

    Updated comments

--- src/p5Setup.js
+++ src/p5Setup.js
@@ -1,3 +1,5 @@
+// The setup function that initializes the p5 code and canvas
+// Automatically called
 function setup() {
     createCanvas(400, 500);
     changePage("screen2");
--- src/pageHandler.js
+++ src/pageHandler.js
@@ -1,5 +1,6 @@
 var currentPage = document.createElement('script');
 
+// For testing purposes
 document.addEventListener("keypress", function(event) {
   if (event.key == 'e') {
     changePage("buttonScreen")
@@ -8,6 +9,11 @@ document.addEventListener("keypress", function(event) {
     changePage("screen1")
   }
 });
+
+
+// Function to switch between page files
+// Inputs:
+//    name- type: string    info: name of page file to switch to (sans .js)
 
 function changePage(name) {
   currentPage.remove();
@@ -22,28 +28,3 @@ function changePage(name) {
   currentPage.id = "p5";
   document.body.appendChild(currentPage);
 }
-
-/*
-var p5LibSection = document.createElement('section');
-var p5minLib = document.createElement('script');
-var p5soundLib;
-
-function resetP5() {
-  p5LibSection.remove();
-  p5minLib.remove();
-
-  p5LibSection = document.createElement('section');
-
-  p5minLib = document.createElement('script');
-  p5minLib.src = "libraries/p5.min.js";
-  p5minLib.id = "p5";
-  
-  p5soundLib = document.createElement('script');
-  p5soundLib.src = "libraries/p5.sound.min.js";
-  p5soundLib.id = "p5";
-
-  p5LibSection.appendChild(p5minLib);
-  p5LibSection.appendChild(p5soundLib);
-
-  document.head.appendChild(p5minLib);
-}*/
